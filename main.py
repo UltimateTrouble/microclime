a5cdebf10f7ea8fac31b1d502e8bb314ca8249e2
Senal Silva <75540329+UltimateTrouble@users.noreply.github.com>
2025-11-12 08:04:47 -0800

    Refactor main.py for cleaner code and readability

--- main.py
+++ main.py
@@ -1,24 +1,17 @@
 import time
 import machine
-from machine import Pin, I2C        # importing relevant modules & classes
+from machine import Pin, I2C        
 from time import sleep
-import bme280                       # importing BME280 library
+import bme280                       
 
-# Set up LED
 led = machine.Pin("LED", machine.Pin.OUT)
 
-# Initialize I2C for BME280
 i2c = I2C(0, sda=Pin(0), scl=Pin(1), freq=400000)
-bme = bme280.BME280(i2c=i2c)  # Initialize BME280 once
+bme = bme280.BME280(i2c=i2c)
 
 while True:
-    # Toggle the LED
     led.toggle()
-    
-    # Read from BME280 sensor and print values
     print(bme.values)
-    
-    # Sleep for a specified time
-    time.sleep(1.5)  # You can adjust this sleep time as needed
+    time.sleep(1.5)
     
 
