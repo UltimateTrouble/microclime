9914d8405d564a982995a844d3d7b7a21094fc6e
Senal Silva <75540329+UltimateTrouble@users.noreply.github.com>
2025-11-13 21:04:32 -0800

    Update data logging with timestamps and fix filename
    Refactor data logging to include timestamps and correct file name.

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 import time
 import machine
 from machine import Pin, I2C        
-from time import sleep
 import bme280                       
 
 led = machine.Pin("LED", machine.Pin.OUT)
-
 i2c = I2C(0, sda=Pin(0), scl=Pin(1), freq=400000)
 bme = bme280.BME280(i2c=i2c)
+datafile = "data.txt"  
 
 while True:
     led.toggle()
-    datafile = "data.text"
-    file = open(datafile, "a")
-    data = bme.values\n
-    file.write(data)
-    file.flush()
-    time.sleep(900)
-    #15 minutes
-
+    data = bme.values
+    timestamp = time.localtime()
+    timestamp_string = f"{timestamp[0]:04}-{timestamp[1]:02}-{timestamp[2]:02} {timestamp[3]:02}:{timestamp[4]:02}:{timestamp[5]:02}"
+    data_string = f"{timestamp_string}, Temperature: {data[0]}, Pressure: {data[1]}, Humidity: {data[2]}\n"
+    with open(datafile, "a") as file:
+        file.write(data_string)
+    time.sleep(1800)  # Sleep for 15 minutes (1800 seconds)
